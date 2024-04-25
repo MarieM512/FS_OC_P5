d6b9791f313540419097cb387c4fdc00c85e1b12
Marie Metay <mariemetay.pro@gmail.com>
2024-04-25 20:37:45 +0200

    Add tests for connect and disconnect

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,9 +2,19 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
+  const mockUser: SessionInformation = {
+    token: "string",
+    type: "string",
+    id: 1,
+    username: "Johnny",
+    firstName: "John",
+    lastName: "Doe",
+    admin: true
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,5 +23,22 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should connected', () => {
+    expect(service.isLogged).toBeFalsy();
+    expect(service.sessionInformation).toBeUndefined();
+
+    service.logIn(mockUser);
+
+    expect(service.isLogged).toBeTruthy();
+    expect(service.sessionInformation).toEqual(mockUser);
+  });
+
+  it('should disconnected', () => {
+    service.logOut();
+
+    expect(service.isLogged).toBeFalsy();
+    expect(service.sessionInformation).toBeUndefined();
   });
 });
