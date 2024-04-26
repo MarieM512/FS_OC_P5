a6e9adcb560d0251b37e92863d20a5b3ba704c6f
Marie Metay <mariemetay.pro@gmail.com>
2024-04-27 01:42:37 +0200

    Add test to display list of sessions

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -6,6 +6,9 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { ListComponent } from './list.component';
+import { SessionApiService } from '../../services/session-api.service';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -17,11 +20,41 @@ describe('ListComponent', () => {
     }
   }
 
+  const mockSessions = [
+    {
+      id: 1,
+      name: 'Session 1',
+      date: new Date(),
+      description: 'Description',
+      users: Array(10).fill({}),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      teacher_id: 1,
+    },
+    {
+      id: 2,
+      name: 'Session 2',
+      date: new Date(),
+      description: 'Description',
+      users: Array(8).fill({}),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      teacher_id: 1,
+    }
+  ]
+
+  const mockSessionApiService = {
+    all: jest.fn().mockReturnValue(of(mockSessions))
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{ provide: SessionService, useValue: mockSessionService }]
+      imports: [HttpClientModule, MatCardModule, MatIconModule, RouterTestingModule],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService }
+      ]
     })
       .compileComponents();
 
@@ -30,7 +63,15 @@ describe('ListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display list of sessions', () => {
+    expect(component.sessions$).toBeTruthy();
+    component.sessions$.subscribe((sessions) => {
+      expect(sessions).toHaveLength(2);
+    });
+
+    const compiled = fixture.nativeElement;
+    const cards = compiled.querySelectorAll('mat-card.item');
+    expect(cards).toHaveLength(2);
+    expect(cards[0].querySelector('mat-card-title').textContent).toContain(mockSessions[0].name);
   });
 });
