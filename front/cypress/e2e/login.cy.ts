168b4d6fed5c4ef4bb973e3997cb41a7c0d3d10f
Marie Metay <mariemetay.pro@gmail.com>
2024-04-27 15:37:50 +0200

    Add tests to display error when wrong password

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -24,4 +24,25 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('Should display error when wrong password', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email').type("yoga@studio.com")
+    cy.get('input[formControlName=password').type(`${"test1234"}{enter}{enter}`)
+
+    cy.get('.error')
+  })
+
+  it('Should disable submit button when empty field', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email').type("yoga@studio.com")
+
+    cy.get(':button').should('be.disabled')
+
+    cy.get('input[formControlName=password').type(`${"test1234"}`)
+
+    cy.get(':button').should('be.not.disabled')
+  })
 });
