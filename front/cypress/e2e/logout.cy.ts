bcde9fb6c11f1f48221dc05f0507eb8941d81843
Marie Metay <mariemetay.pro@gmail.com>
2024-04-27 19:22:52 +0200

    Clean test logout

--- front/cypress/e2e/logout.cy.ts
+++ front/cypress/e2e/logout.cy.ts
@@ -1,31 +1,9 @@
 describe('Logout spec', () => {
     it('Logout successfull', () => {
-        cy.visit('/login')
+        cy.login(true, false)
 
-        cy.intercept('POST', '/api/auth/login', {
-            body: {
-              id: 1,
-              username: 'userName',
-              firstName: 'firstName',
-              lastName: 'lastName',
-              admin: true
-            },
-          })
-      
-          cy.intercept(
-            {
-              method: 'GET',
-              url: '/api/session',
-            },
-            []).as('session')
-      
-          cy.get('input[formControlName=email]').type("yoga@studio.com")
-          cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-      
-          cy.url().should('include', '/sessions')
+        cy.get('.link').contains('Logout').click()
 
-          cy.get('.link').contains('Logout').click()
-
-          cy.url().should('include', '')
+        cy.url().should('include', '')
     })
   });
