b5258a678cca9b1319f88c98a5d57a2812d7f2ec
Marie Metay <mariemetay.pro@gmail.com>
2024-04-27 19:24:20 +0200

    Clean test register

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,15 +1,15 @@
 describe('Register spec', () => {
     it('Register successfull', () => {
-      cy.visit('/register')
+        cy.visit('/register')
 
-      cy.intercept('POST', '/api/auth/register', {})
+        cy.intercept('POST', '/api/auth/register', {})
   
-      cy.get('input[formControlName=firstName]').type("John")
-      cy.get('input[formControlName=lastName]').type("Doe")
-      cy.get('input[formControlName=email]').type("test@oc.com")
-      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}`)
+        cy.get('input[formControlName=firstName]').type("John")
+        cy.get('input[formControlName=lastName]').type("Doe")
+        cy.get('input[formControlName=email]').type("test@oc.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}`)
   
-      cy.url().should('include', '/login')
+        cy.url().should('include', '/login')
     })
 
     it('Should disable submit button when empty field', () => {
@@ -24,5 +24,5 @@ describe('Register spec', () => {
         cy.get('input[formControlName=lastName').type("Doe")
 
         cy.get(':button').should('be.not.disabled')
-      })
+    })
   });
