652a83012ac006479f862a136c79fcd43f3b09e4
Marie Metay <mariemetay.pro@gmail.com>
2024-04-27 15:58:49 +0200

    Add test to disable button if empty field in register

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -11,4 +11,18 @@ describe('Register spec', () => {
   
       cy.url().should('include', '/login')
     })
+
+    it('Should disable submit button when empty field', () => {
+        cy.visit('/register')
+    
+        cy.get('input[formControlName=email').type("yoga@studio.com")
+        cy.get('input[formControlName=password').type("test!1234")
+
+        cy.get(':button').should('be.disabled')
+    
+        cy.get('input[formControlName=firstName').type("John")
+        cy.get('input[formControlName=lastName').type("Doe")
+
+        cy.get(':button').should('be.not.disabled')
+      })
   });
